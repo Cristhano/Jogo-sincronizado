dec8fc16f01e6618b79110db1635fe3f9bff65a3
Cristiao <cr7709867@gmail.com>
2026-06-04 18:38:56 -0300

    v3

---
+++ public/KeybordListenes.js
@@ -0,0 +1,35 @@
+export default function createKeybordListenes(document) {
+    const state = {
+        obsevers: [], //observadores
+        playerId: null
+    }
+    function registerPlayerId(playerId){
+        state.playerId = playerId
+    }
+
+    function subscribe(ObseverFunction) { // Registrar Observador
+        state.obsevers.push(ObseverFunction)
+    }
+    function NotifyAll(command) {
+        for (const ObseverFunction of state.obsevers) {
+            ObseverFunction(command)
+        }
+    }
+
+    document.addEventListener("keydown", handleKeyDown) //input
+
+    function handleKeyDown(event) {
+        const keyPressed = event.key
+        const command = {
+            type: 'move-player',
+            playerId: state.playerId,
+            keyPressed
+        }
+        NotifyAll(command)
+    }
+
+    return {
+        subscribe,
+        registerPlayerId
+    }
+}
---
+++ sever.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import http from 'http'
+import CreateGame from './public/game.js'
+import { Server } from 'socket.io'
+import { Pool } from 'pg' //biblioteca postegre com = npm install pg
+
+const app = express()
+const sever = http.createServer(app)
+const sockets = new Server(sever)
+
+app.use(express.static('public'))
+
+const game = CreateGame()
+
+game.subscribe((command) => {
+    sockets.emit(command.type, command)
+    sockets.emit('setup', game.state)
+})
+
+sockets.on('connection', (socket) => {
+    const playerId = socket.id
+
+    console.log('Jogador conectado no servidor com o id: ' + playerId)
+
+    game.addPlayer({ playerId })
+
+    socket.on('disconnect', () => {
+        console.log('Jogador desconectado:', playerId)
+        game.removeplayer({playerId:playerId})
+    })
+})
+
+sever.listen(1650, () => {
+    console.log('Sever rodando na porta: 1650')
+})
